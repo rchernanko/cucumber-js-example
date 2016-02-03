434ff310d149fbc73c96086da422f407a016f40e
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-02-03 11:02:33 +0000

    initial commit, including gitignore file, feature file and step defs

---
+++ features/step_definitions/add-item.steps.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function () {
+
+    this.Given(/^I have an empty grocery list$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+    this.When(/^I add an item to the list$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+    this.Then(/^The grocery list contains a single item$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+    this.Then(/^I can access that item from the grocery list$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+};
