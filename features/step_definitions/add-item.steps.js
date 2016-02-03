5a97d6277912494658917c925def302e752afabe
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-02-03 11:16:08 +0000

    given step being recognised but failing - about to add support code

--- features/step_definitions/add-item.steps.js
+++ features/step_definitions/add-item.steps.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var GroceryList = require(process.cwd() + '/script/model/grocery-list');
+
 module.exports = function () {
 
+    var myList;
+
     this.Given(/^I have an empty grocery list$/, function (callback) {
-        // Write code here that turns the phrase above into concrete actions
-        callback.pending();
+        myList = GroceryList.create();
+        callback();
     });
 
     this.When(/^I add an item to the list$/, function (callback) {
